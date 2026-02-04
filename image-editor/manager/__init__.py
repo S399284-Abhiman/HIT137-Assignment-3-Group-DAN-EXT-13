b5955a7b5bf5b7c81fa265166149e59b1b568f3b
S399284-Abhiman <abhiman.bhattarai@students.cdu.edu.au>
2026-02-04 22:27:00 +0930

    update

--- image-editor/manager/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Managers Package
-
-This package contains manager classes that coordinate application logic.
-"""
-
-from .filter_manager import FilterManager
-
-__all__ = ['FilterManager']
---
+++ image-editor/managers/__init__.py
@@ -0,0 +1,9 @@
+"""
+Managers Package
+
+This package contains manager classes that coordinate application logic.
+"""
+
+from .filter_manager import FilterManager
+
+__all__ = ['FilterManager']
